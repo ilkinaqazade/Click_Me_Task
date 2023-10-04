56923daeaf78fd9757c6cc0eae90573a237d9215
admin <admin@DESKTOP-NFU6TKL>
2023-10-04 14:28:47 +0400

    Add project files.

---
+++ 2 task/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace _2_task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonBt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonBt
+            // 
+            this.buttonBt.BackgroundImage = global::_2_task.Properties.Resources.lotu;
+            this.buttonBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonBt.Location = new System.Drawing.Point(280, 145);
+            this.buttonBt.Name = "buttonBt";
+            this.buttonBt.Size = new System.Drawing.Size(144, 62);
+            this.buttonBt.TabIndex = 0;
+            this.buttonBt.UseVisualStyleBackColor = true;
+            this.buttonBt.MouseEnter += new System.EventHandler(this.buttonBt_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(209, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 75);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lotunu Tut";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBt;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2 task/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBt_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int x = random.Next(0,500);
+            int y = random.Next(0,400);
+            buttonBt.Left = x;
+            buttonBt.Top = y;
+        }
+
+
+    }
+}
